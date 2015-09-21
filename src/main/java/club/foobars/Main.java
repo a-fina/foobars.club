407de079cc276b5fa7dfe50ae23f714115f059ac
bayois@gmail.com <>
2015-09-21 10:08:11 +0200

    Aggiunto j2html

--- src/main/java/club/foobars/Main.java
+++ src/main/java/club/foobars/Main.java
@@ -7,6 +7,11 @@ package club.foobars;
 
 import static spark.Spark.*;
 import static j2html.TagCreator.*;
+import java.util.HashMap;
+import java.util.Map;
+import spark.ModelAndView;
+import spark.template.freemarker.FreeMarkerEngine;import spark.template.mustache.MustacheTemplateEngine;
+;
 
 /**
  *
@@ -18,18 +23,26 @@ public class Main {
         staticFileLocation("/public");
         port(getHerokuAssignedPort());
 
+
+        Map map = new HashMap();
+        map.put("name", "Sam");
+        // hello.mustache file is in resources/templates directory
+        get("/mustache", (rq, rs) -> 
+                new ModelAndView(map, "hello.mustache"), 
+                new MustacheTemplateEngine()
+        );
+        
+
         get("/hello", (req, res) -> "Hello Heroku World");
 
         get("/foobar.club", (req, res) -> {
             return html().with(
                     head().with(
-                            title("Title"),
-                            link().withRel("stylesheet").withHref("/css/main.css")
+                            title("Foo, Bars & Wheels - Club"),
+                            link().withRel("stylesheet").withHref("css/main.css")
                     ),
                     body().with(
-                            div().with(
-                                    h1("Heading!")
-                            )
+                        img().withSrc("img/costina-mtb-big.png")
                     )
             ).toString();
         });
